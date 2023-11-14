711aab49b37f81e9cef1937708448fa64f2311ec
Amauri Rodrigues <amauriaureo@gmail.com>
2023-11-13 21:49:05 -0300

    utilizando Waitress como servidor WSGI

---
+++ ambiente_de_producao.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def bom_dia():
+    return "Bom dia"
+
+if __name__ == '__main__':
+    from waitress import serve
+    serve(app, host = "0.0.0.0", port = 8768)
