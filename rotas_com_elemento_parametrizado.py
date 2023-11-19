0291dd6ab766e8a3eb074a84a4c42b03418295fd
Amauri Rodrigues <amauriaureo@gmail.com>
2023-11-19 10:54:22 -0300

    dicionario

--- rotas_com_elemento_parametrizado.py
+++ rotas_com_elemento_parametrizado.py
@@ -1,3 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+cores_frutas = {
+    "morango": "vermelho",
+    "uva": "roxo",
+    "banana": "amarelo",
+    "abacaxi": "amarelo",
+    "limao": "verde",
+}
