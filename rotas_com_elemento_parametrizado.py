1657d3ad0e4d5c568eb8580b18027170010f4cb0
Amauri Rodrigues <amauriaureo@gmail.com>
2023-11-19 10:56:06 -0300

    funcao

--- rotas_com_elemento_parametrizado.py
+++ rotas_com_elemento_parametrizado.py
@@ -9,4 +9,9 @@ cores_frutas = {
     "limao": "verde",
 }
 
+
 @app.route("/frutas/<nome_fruta>/cor")
+def frutas(nome_fruta):
+    if nome_fruta in cores_frutas:
+        return cores_frutas[nome_fruta]
+    return "Não sei"
